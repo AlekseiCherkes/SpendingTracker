61c43b516ac7745125777a3e33bb4107c38641f8
Aliaksei Charkes <aliaksei.charkes@mapbox.com>
2024-01-05 18:43:35 +0300

    Initial Commit

---
+++ SpendingTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpendingTracker
+//
+//  Created by Aliaksei Charkes on 5.1.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpendingTracker/SpendingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendingTrackerApp.swift
+//  SpendingTracker
+//
+//  Created by Aliaksei Charkes on 5.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct SpendingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
